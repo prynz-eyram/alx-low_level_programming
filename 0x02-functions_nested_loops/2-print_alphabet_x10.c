df13d5df72af1cecf57a7f5725ea8e77da916be2
prynz-eyram <88299605+prynz-eyram@users.noreply.github.com>
2021-08-31 18:15:33 +0000

    10 x alphabet

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,26 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - prints alphabets 10 times, followed by a new line
+ *
+ */
+
+
+
+void print_alphabet_x10(void)
+{
+	 char x;
+ 	 int count = 0;
+ 	while (count < 10)
+	{
+		for (x = 97; x <= 122; x++)
+		{
+			_putchar(x);
+		}
+		_putchar('\n');
+		count++;
+	}
+
+
+
+}
