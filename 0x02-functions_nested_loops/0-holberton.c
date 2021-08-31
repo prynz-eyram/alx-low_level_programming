f7baf84686411453175d82ede582f5a60ce14e6a
prynz-eyram <88299605+prynz-eyram@users.noreply.github.com>
2021-08-31 17:27:52 +0000

    _putchar

---
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * main - prints 'Holberton' followed by a new line
+ *
+ * Return: Always 0 (Success)
+ */
+
+
+int main(void)
+{
+	int i;
+	char p[] = "Holberton";
+
+	for (i = 0; i < 9; i++)
+	{
+	_putchar(p[i]);
+	}
+	_putchar('\n');
+	return (0);
+}
