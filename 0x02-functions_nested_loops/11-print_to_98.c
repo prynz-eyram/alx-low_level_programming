c3211ce0059e24dbb964873c974aa5d4f2d307b7
prynz-eyram <88299605+prynz-eyram@users.noreply.github.com>
2021-09-02 10:41:50 +0000

    Holberton School, 98 Battery Street, the OG

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,40 @@
+#include "main.h"
+#include <stdio.h>
+
+/**
+ * print_to_98 -  prints natureal  numbers from n to 98
+ * @n: takes an integer
+ *
+ */
+
+void print_to_98(int n)
+{
+	int a;
+
+	if (n < 98)
+	{
+		for (a = n; a <= 98; a++)
+		{
+			printf("%d", a);
+			if (a != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	else
+	{
+		for (a = n; a >= 98; a--)
+		{
+			printf("%d", a);
+			if (a != 98)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	
+	putchar('\n');
+}
