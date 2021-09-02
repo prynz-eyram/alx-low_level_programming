8c98fd3278e1dcaf9335785a2adcde81d198d82a
prynz-eyram <88299605+prynz-eyram@users.noreply.github.com>
2021-09-02 20:36:29 +0000

    You are so much sunshine in every square inch

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,12 +14,13 @@ void print_square(int size)
 	{
 		int i, j;
 
-		for (i = 0; i < size; i++){
-																	for (j = 0; j < size; j++)
-																					{
-																										_putchar('#');
-																													}
-																				_putchar('\n');
-																						}
-											}
+		for (i = 0; i < size; i++)
+		{
+			for (j = 0; j < size; j++)
+			{
+				_putchar('#');
+			}
+			_putchar('\n');
+		}
+	}
 }
