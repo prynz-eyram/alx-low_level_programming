e04b38f25fd74c171c008adaa2251be2c692ffa7
prynz-eyram <88299605+prynz-eyram@users.noreply.github.com>
2021-09-17 19:03:12 +0000

    The Great Commit

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+/**
+ * main - program that prints its name followed by a new line
+ * @argc: argument count
+ * @argv: array of string  arguments
+ * Return: Always 0
+ */
+
+int main(int argc, char *argv[])
+{
+	argc = argc;
+	printf("%s\n", *argv);
+	return (0);
+}
