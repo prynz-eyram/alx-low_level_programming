6082cfe1dd72e865c99073198684c72d32ee89bc
prynz-eyram <88299605+prynz-eyram@users.noreply.github.com>
2021-09-27 17:57:11 +0000

    The Great Commit

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,28 @@
+#include "main.h"
+#include <stdlib.h>
+
+
+/**
+ * _calloc - allocates memory for an array
+ * @nmemb: number of elements
+ * @size: size of each element
+ * Return: pointer to allocated memory else NULL
+*/
+
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *mem;
+	unsigned int i;
+
+	if (!nmemb || !size)
+		return (NULL);
+
+	mem = malloc(nmemb * size);
+	if (mem == NULL)
+		return (NULL);
+
+	for (i = 0; i < nmemb * size; i++)
+		mem[i] = 0;
+
+	return (mem);
+}
