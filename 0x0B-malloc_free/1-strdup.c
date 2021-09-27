2a24243bf52061cee5015865ffe8f0805e03a4c6
prynz-eyram <88299605+prynz-eyram@users.noreply.github.com>
2021-09-27 17:50:17 +0000

    The Great Commit

---
+++ 0x0B-malloc_free/1-strdup.c
@@ -0,0 +1,35 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stddef.h>
+
+/**
+ * _strdup - returns a pointer to a newly allocated space in memory,
+ * which contains a copy of the string given as parameter
+ * @str: number of bytes to be allocated in memory
+ * Return: pointer to duplicate array if successful else NULL
+ */
+
+char *_strdup(char *str)
+{
+	int i, j;
+	char *copy;
+
+	if (str != NULL)
+	{
+
+		for (i = 0; str[i] != '\0'; i++)
+			;
+		i = i + 1;
+
+		copy = malloc(sizeof(char) * i);
+		if (copy == NULL)
+			return (NULL);
+
+		for (j = 0; str[j] != '\0'; j++)
+			copy[j] = str[j];
+
+		return (copy);
+	}
+	else
+		return (NULL);
+}
