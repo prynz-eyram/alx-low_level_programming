7e970ed6a316cb1f22b8ebd029de4a80455a4792
prynz-eyram <88299605+prynz-eyram@users.noreply.github.com>
2021-10-12 22:44:18 +0000

    Update

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,9 +12,10 @@ void free_listint2(listint_t **head)
 
 	while (*head)
 	{
-		current = *head;
-		(*head) = (*head)->next;
-		free(current);
+		current = (*head)->next;
+		free(*head);
+		*head = current;
 	}
 
+	*head = NULL;
 }
---
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -0,0 +1,49 @@
+#include "lists.h"
+
+
+/**
+ * insert_nodeint_at_index -  frees a listint_t list
+ *
+ * @head: pointer to a pointer of type listint_t
+ * @idx: index of the list where new node should be added.
+ * @n: data
+ * Return: address of new node if successful else NULL
+*/
+
+listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
+{
+	listint_t *current = *head;
+	listint_t *temp;
+	listint_t *new_node;
+	unsigned int count = 0;
+
+	new_node = malloc(sizeof(listint_t));
+	if (!new_node)
+		return (NULL);
+
+	new_node->n = n;
+	if (idx == 0)
+	{
+		new_node->next = *head;
+		(*head) = new_node;
+		return (new_node);
+	}
+
+	while (current && idx > count)
+	{
+		if (count == idx - 1)
+		{
+			temp = current->next;
+			current->next = new_node;
+			new_node->next = temp;
+
+			return (new_node);
+		}
+		else
+			current = current->next;
+		count++;
+	}
+
+	return (NULL);
+
+}
